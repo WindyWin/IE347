0a9c05e0d8df514224a4f95f27611bc88649c5c1
WindyWind <19521362@gm.uit.edu.vn>
2022-12-20 10:05:24 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import "antd/dist/antd.min.css";
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Loader } from "./components/core";
-import { blogData } from "./modules/utils/DataGenerate";
 import RouterView from "./router/router";
 import { handelRecursion } from "./router/type";
 import "./sass/index.scss";
 function App() {
-  console.table(blogData);
 
   return (
     <div className="App">
