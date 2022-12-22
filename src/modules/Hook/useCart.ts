c52411bb81191ea7827cca5d0e7a53b376eb1b83
WindyWind <19521362@gm.uit.edu.vn>
2022-12-22 18:53:26 +0700

    noob custom hook

---
+++ src/modules/Hook/useCart.ts
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import cartContext from "../Context/cartContext";
+// import { message } from "antd";
+
+
+const useCart = () => {
+    let cart = useContext(cartContext);
+
+    return {
+
+        getCart: cart,
+        addToCart: (product: any) => {
+            if (!!cart.find((item: any) => item._id === product._id)) {
+                return;
+            }
+            cart.push(product);
+
+            window.localStorage.setItem("products", JSON.stringify(cart));
+        },
+        removeFromCart: (product: any) => {
+            cart = cart.filter((item: any) => item._id !== product._id);
+            window.localStorage.setItem("products", JSON.stringify(cart));
+        },
+        getCartLength: (): number => {
+            return cart.length;
+        },
+        updateQuantity: (product: any, quantity: number) => {
+            if (quantity >= 0) {
+                let index = cart.findIndex((item: any) => item._id === product._id);
+                cart[index].quantity = quantity;
+                window.localStorage.setItem("products", JSON.stringify(cart));
+                return;
+
+            }
+
+            cart = cart.filter((item: any) => item._id !== product._id);
+            window.localStorage.setItem("products", JSON.stringify(cart));
+        },
+    }
+}
+
+
+export default useCart;
