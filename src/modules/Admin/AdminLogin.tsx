1bbef35efeb4bd23ee1e2c6e84c8f326a78b9b62
WindyWind <19521362@gm.uit.edu.vn>
2022-12-22 18:54:55 +0700

    notification for admin layout

--- src/modules/Admin/AdminLogin.tsx
+++ src/modules/Admin/AdminLogin.tsx
@@ -1,40 +1,66 @@
-import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Dropdown, message, Space } from "antd";
+import { BellOutlined, CheckCircleOutlined, CloseCircleOutlined, UserOutlined } from "@ant-design/icons";
+import { Avatar, Badge, Button, Col, Dropdown, Menu, message, Row, Space } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import AdminLogout from "../../components/core/AdminLogout";
 
+const notifications = [
+  {
+    type: "success",
+    message: "Đơn hàng đã được xác nhận",
+    description: "Đơn hàng đã được xác nhận và đang được giao",
+
+  },
+  {
+    type: "error",
+    message: "Đơn hàng đã bị hủy",
+    description: "Đơn hàng đã bị hủy",
+  }
+]
+
 function AdminLogin() {
   const navigate = useNavigate();
+
   return (
     <>
-      {/* {isAdmin ? (
-        <>
-          <div
-            style={{
-              display: "inline-block",
-              position: "absolute",
-              right: "0",
-              marginRight: "2%",
-              minWidth: "8%",
-            }}
-          >
-            <Dropdown overlay={<AdminLogout />}>
-              <Space>
-                <Avatar
-                  style={{
-                    color: "#f56a00",
-                    backgroundColor: "#fde3cf",
-                    marginRight: "10px",
-                  }}
-                  icon={<UserOutlined />}
-                />
-                {JWTManager.getUsername()}
-              </Space>
+      {/* {isAdmin ? ( */}
+      <>
+        <div
+          className="admin__header-right"
+        >
+          <Badge count={2}
+          ><Dropdown overlay={
+            <Menu>
+              {notifications.map(
+                (item: any, index: any) =>
+                  <Menu.Item key={index}
+                    onClick={() => { message.success(item.message) }}>
+                    <SimpleNotification type={item.type} message={item.message} description={item.description} />
+                  </Menu.Item>)}
+            </Menu>
+          }>
+              <div className="noti-button">
+                <BellOutlined />
+              </div>
             </Dropdown>
-          </div>
-        </>
-      ) : (
+          </Badge>
+
+          <Dropdown overlay={<AdminLogout />}>
+            <Space>
+              <Avatar
+                style={{
+                  color: "#f56a00",
+                  backgroundColor: "#fde3cf",
+                  marginRight: "10px",
+                }}
+                icon={<UserOutlined />}
+              />
+              Admin
+            </Space>
+          </Dropdown>
+        </div>
+      </>
+      {/* ) : (
         ""
       )} */}
     </>
@@ -42,3 +68,26 @@ function AdminLogin() {
 }
 
 export default AdminLogin;
+
+//create notication menu components
+
+
+function SimpleNotification({ type, message, description }: any) {
+  return <>
+    <Row style={{ width: "250px" }}>
+      <Col span={4}>
+        {type === "success" ? (
+          <CheckCircleOutlined style={{ color: "green", fontSize: "30px" }} />
+        ) : (
+          <CloseCircleOutlined style={{ color: "red", fontSize: "30px" }} />
+        )}
+
+      </Col>
+      <Col span={20}>
+        <h4>{message}</h4>
+        <p>{description}</p>
+      </Col>
+    </Row>
+  </>
+
+}
